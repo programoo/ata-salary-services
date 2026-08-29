96b7fbebdd875259e480230941953fadc1b4073f
prakarn-pc <prakarnwongsanit@gmail.com>
2026-08-29 08:29:25 +0700

    Support status OK when calling to root path

---
+++ src/main/java/com/ata/salaryservices/controller/RootController.java
@@ -0,0 +1,15 @@
+package com.ata.salaryservices.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public Map<String, String> root() {
+        return Map.of("status", "OK");
+    }
+}
